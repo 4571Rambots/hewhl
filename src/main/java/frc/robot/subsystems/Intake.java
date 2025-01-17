8cceee301770130692fa782b4e3171067ece2040
4571Rambots <adom.martin@gmail.com>
2025-01-17 17:10:05 -0500

    chore: format intake subsystem

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -7,8 +7,8 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
-  /** Creates a new Intake. */
-  public Intake() {
-      
-  }
+    /** Creates a new Intake. */
+    public Intake() {
+
+    }
 }
