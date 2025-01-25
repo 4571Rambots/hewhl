eb6c58381ec550589d8ebef0c7df9bffa84ea447
4571Rambots <adom.martin@gmail.com>
2025-01-25 15:01:02 -0500

    feat: add xbox controller

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,75 +1,88 @@
-// Copyright (c) FIRST and other WPILib contributors.
+//Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+// import com.revrobotics.spark.SparkBase.ResetMode;
+// import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+// import com.revrobotics.REVLibError;
+import com.revrobotics.RelativeEncoder;
 
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
+    // instance variables for motor and controller
+    private SparkMax neoMotor; // neo Motor via Spark Max
+    private RelativeEncoder neoEncoder;
+    private Joystick xboxController; 
 
-  private final RobotContainer m_robotContainer;
+    // constants for xbox controller button/axis
 
-  public Robot() {
-    m_robotContainer = new RobotContainer();
-  }
+    @Override 
+    public void robotInit() {
+        System.out.println("Initializing robot...");
+        
+        try {
+            // Spark Max controller on CAN ID 5
+            neoMotor = new SparkMax(5, MotorType.kBrushless);
+            neoEncoder = neoMotor.getEncoder();
+            
+            // Create and apply configuration
+            SparkMaxConfig config = new SparkMaxConfig();
+            config.idleMode(IdleMode.kBrake);
+            
+            // Configure and persist settings
+            // REVLibError error = neoMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+            
+            // Print initialization info
+            // System.out.println("=== Motor Initialization ===");
+            // System.out.println("Configuration result: " + error);
+            // System.out.println("Device ID: " + neoMotor.getDeviceId());
+            // System.out.println("Firmware version: " + neoMotor.getFirmwareString());
+            // System.out.println("Bus voltage: " + neoMotor.getBusVoltage());
+            // System.out.println("Has faults: " + neoMotor.hasStickyFault());
+            // System.out.println("Encoder: " + neoEncoder.toString());
+            xboxController = new Joystick(0);
 
-  @Override
-  public void robotPeriodic() {
-    CommandScheduler.getInstance().run(); 
-  }
-
-  @Override
-  public void disabledInit() {}
-
-  @Override
-  public void disabledPeriodic() {}
-
-  @Override
-  public void disabledExit() {}
-
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
+            if (neoMotor.hasStickyFault()) {
+                System.out.println("Sticky faults: " + neoMotor.getStickyFaults());
+                neoMotor.clearFaults(); // Try to clear any faults
+            }
+            
+            neoEncoder.setPosition(0);
+        } catch (Exception e) {
+            System.out.println("ERROR during initialization: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
-  }
+    @Override 
+    public void teleopPeriodic() {
+        try {
+            boolean isYPressed = xboxController.getRawButton(4);
 
-  @Override
-  public void autonomousPeriodic() {}
-
-  @Override
-  public void autonomousExit() {}
-
-  @Override
-  public void teleopInit() {
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
+            neoMotor.set(isYPressed ? 0.02 : 0);
+            
+            // Print detailed diagnostics
+            // System.out.println("=== Motor Status ===");
+            // System.out.println("Command sent: " + neoMotor.get());
+            // System.out.println("Applied output: " + neoMotor.getAppliedOutput());
+            // System.out.println("Bus voltage: " + neoMotor.getBusVoltage());
+            // System.out.println("Output current: " + neoMotor.getOutputCurrent());
+            // System.out.println("Temperature: " + neoMotor.getMotorTemperature());
+            
+            // Check for active faults
+            if (neoMotor.hasActiveFault()) {
+                System.out.println("ACTIVE FAULTS DETECTED:");
+                System.out.println(neoMotor.getFaults());
+            }
+            
+        } catch (Exception e) {
+            System.out.println("ERROR during teleop: " + e.getMessage());
+        }
     }
-  }
-
-  @Override
-  public void teleopPeriodic() {}
-
-  @Override
-  public void teleopExit() {}
-
-  @Override
-  public void testInit() {
-    CommandScheduler.getInstance().cancelAll();
-  }
-
-  @Override
-  public void testPeriodic() {}
-
-  @Override
-  public void testExit() {}
-
-  @Override
-  public void simulationPeriodic() {}
 }
